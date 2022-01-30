ff1ebfe7bf16832c318dd142963fb568db50fc26
davidberiro <deebenchimol@gmail.com>
2022-01-30 12:57:52 -0500

    removed cap

--- contracts/PreSale.sol
+++ contracts/PreSale.sol
@@ -8,33 +8,23 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract PreSale is Ownable {
 
     uint public bnbPerToken;
-    uint public maxBnbPerUser;
     address public tokenAddress;
 
     mapping (address => bool) public whitelist;
-    mapping (address => uint) public bnbSent;
 
     constructor(
         uint _bnbPerToken,
-        uint _maxBnbPerUser,
         address _tokenAddress
     ) {
         bnbPerToken = _bnbPerToken;
-        maxBnbPerUser = _maxBnbPerUser;
         tokenAddress = _tokenAddress;
     }
 
     receive() external payable {
         require(whitelist[msg.sender], "whitelist");
-        require(bnbSent[msg.sender] + msg.value <= maxBnbPerUser, "bnb cap");
         
-        bnbSent[msg.sender] += msg.value;
         uint tokenAmount = (msg.value * 1e18) / bnbPerToken;
         IERC20(tokenAddress).transfer(msg.sender, tokenAmount);
-    }
-
-    function setMaxBnbPerUser(uint _maxBnbPerUser) external onlyOwner {
-        maxBnbPerUser = _maxBnbPerUser;
     }
 
     function addWhitelist(address[] calldata addresses) external {
